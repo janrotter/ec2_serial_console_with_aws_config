6150dae34a893b8d195bfe43bd8b8819c1875f57
Jan Rotter <12612258+janrotter@users.noreply.github.com>
2024-05-26 22:06:14 +0200

    pre-commit run --all-files

--- cdk/tests/unit/test_serialconsole_config_stack.py
+++ cdk/tests/unit/test_serialconsole_config_stack.py
@@ -3,12 +3,14 @@ import aws_cdk.assertions as assertions
 
 from serialconsole_config.serialconsole_config_stack import SerialconsoleConfigStack
 
+
 # example tests. To run these tests, uncomment this file along with the example
 # resource in serialconsole_config/serialconsole_config_stack.py
 def test_sqs_queue_created():
     app = core.App()
     stack = SerialconsoleConfigStack(app, "serialconsole-config")
     template = assertions.Template.from_stack(stack)
+
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
